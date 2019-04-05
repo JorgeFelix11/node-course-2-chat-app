1e91e6f96d6609656cfe32b2fc53ae3955ad5df8
Felix <jorge.felix2741@gmail.com>
2019-04-05 12:48:02 -0700

    Add connect and disconnect event handlers

--- server/server.js
+++ server/server.js
@@ -5,15 +5,20 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 var path_1 = __importDefault(require("path"));
 var express_1 = __importDefault(require("express"));
+var socket_io_1 = __importDefault(require("socket.io"));
+var http_1 = __importDefault(require("http"));
 var app = express_1.default();
 var port = process.env.PORT || 3000;
 var publicPath = path_1.default.join(__dirname, '../public');
+var server = http_1.default.createServer(app);
+var io = socket_io_1.default(server);
+io.on('connection', function (socket) {
+    console.log('New user connected');
+    socket.on('disconnect', function () {
+        console.log('User was disconnected');
+    });
+});
 app.use(express_1.default.static(publicPath));
-console.log(__dirname + '/../public');
-console.log(publicPath);
-// app.get('/', (req,res) => {
-//   res.send('<h1>Hello</h1>')
-// })
-app.listen(port, function () {
+server.listen(port, function () {
     console.log("Server listening to port " + port);
 });
--- server/server.ts
+++ server/server.ts
@@ -1,17 +1,23 @@
 import path from 'path';
 import express from 'express';
+import socketIO from 'socket.io';
+import http from 'http';
 
 let app = express();
 const port = process.env.PORT || 3000;
 const publicPath = path.join(__dirname, '../public');
+let server = http.createServer(app);
+let io = socketIO(server)
+
+io.on('connection', socket => {
+  console.log('New user connected')
+  socket.on('disconnect', () => {
+    console.log('User was disconnected')
+  })
+})
+
 app.use(express.static(publicPath))
-console.log(__dirname + '/../public');
-console.log(publicPath);
 
-// app.get('/', (req,res) => {
-//   res.send('<h1>Hello</h1>')
-// })
-
-app.listen(port,()=>{
+server.listen(port,()=>{
   console.log(`Server listening to port ${port}`)
 })
